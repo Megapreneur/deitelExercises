cbb5b1e7680f55c34cfc81e0196cb6d931a63d64
Megapreneur <ademolamegbabi@gmail.com>
2022-06-29 13:25:29 +0100

    Updated work

---
+++ src/LoveQuestions/PeakNumberInANArray.java
@@ -0,0 +1,23 @@
+package LoveQuestions;
+
+public class PeakNumberInANArray {
+    private static int peakValue;
+    public static void main(String[] args) {
+        int[] nums = {10, 8 ,6, 5, 3, 2};
+        System.out.println(findPeak(nums));
+
+
+    }
+    public static int findPeak(int [] arr){
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]){
+                peakValue = arr[i];
+            } else if (arr[0] > arr[1]) {
+                peakValue = arr[0];
+            } else if (arr[arr.length - 1] > arr[arr.length - 2]) {
+                peakValue = arr[arr.length - 1];
+            }
+        }
+        return peakValue;
+    }
+}
--- src/LoveQuestions/ResortingAnArray.java
+++ src/LoveQuestions/ResortingAnArray.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 
 public class ResortingAnArray {
     public static void main(String[] args) {
-        int[] nums = {4, 0, 1, 0, 12, 7, 0};
+        int[] nums = {0,0,0,4, 0, 1, 0, 12, 7, 0};
         rearrangeArray(nums);
     }
     public static void rearrangeArray(int[] arr) {
-        int[] newArray = new int[5];
+//        int[] newArray = new int[arr.length];
         int a = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
-                newArray[a] = arr[i];
+                arr[a] = arr[i];
                 ++a;
             }
         }
-        System.out.println(Arrays.toString(newArray));
+        System.out.println(Arrays.toString(arr));
     }
 }
