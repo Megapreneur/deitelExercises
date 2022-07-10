ff6e631c4ae70b2cd3f706fe8cffec7c1135a14c
Megapreneur <ademolamegbabi@gmail.com>
2022-07-10 21:20:52 +0100

    Updated work

--- src/PersonalTest2/GeneratingAccountNumber.java
+++ src/PersonalTest2/GeneratingAccountNumber.java
@@ -1,7 +1,22 @@
 package PersonalTest2;
 
+import java.security.SecureRandom;
+
 public class GeneratingAccountNumber {
     public static void main(String[] args) {
+        String number = "0123456789";
+
+        int len = 10;
+
+        char[] accountNumber = new char[len];
+        SecureRandom random = new SecureRandom();
+
+        for (int i = 0; i < len; i++) {
+            accountNumber[i] = number.charAt(random.nextInt(number.length()));
+
+        }
+        System.out.println("Generated password is:" + new String(accountNumber));
+
 
     }
 }
