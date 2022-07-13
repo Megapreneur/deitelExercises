8df916123a8311e4f96b32ea65e3b6281513ab50
Megapreneur <ademolamegbabi@gmail.com>
2022-07-13 18:59:48 +0100

    Updated work

---
+++ src/LeetCode/MedianOfTwoSortedArrays.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,7};
+        int[] arr1 = {2,4,6,8};
+
+        int a = arr.length;
+        int b = arr1.length;
+
+        int c = a + b;
+
+        int[] newArray = new int[c];
+
+        System.arraycopy(arr, 0, newArray, 0, a);
+        System.arraycopy(arr1, 0, newArray, a, b);
+        Arrays.sort(newArray);
+
+        for (int i = 0; i < newArray.length; i++) {
+            if (){
+
+            }
+
+        }
+    }
+}
