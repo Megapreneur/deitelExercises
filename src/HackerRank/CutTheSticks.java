802c11651b9f044a8e3e98333d30b64832991cfb
Megapreneur <ademolamegbabi@gmail.com>
2022-07-21 11:12:37 +0100

    Updated work

--- src/HackerRank/CutTheSticks.java
+++ src/HackerRank/CutTheSticks.java
@@ -7,7 +7,7 @@ public class CutTheSticks {
 
 
     }
-    public static void cutting(List<Integer> arr){
+    public static List<Integer> cutting(List<Integer> arr){
         Collections.sort(arr);
         Map<Integer, Integer> map = new LinkedHashMap<>();
 //        fill up with the linkedHashmap with the count of each element of the sorted array
@@ -21,14 +21,19 @@ public class CutTheSticks {
 //        System.out.println(map.entrySet());
         List<Integer> list = new ArrayList<>();
         int size = arr.size();
+        int mapSize = map.size();
+        int count = 0;
+
         list.add(size);
 
         for (Map.Entry entry: map.entrySet()) {
-            if (count !=)
-            size = size - (int)entry.getValue();
-            list.add(size);
-            count++;
+            if (count != map.size() - 1){
+                size = size - (int)entry.getValue();
+                list.add(size);
+                count++;
+            }
         }
+        return list;
 
     }
 }
