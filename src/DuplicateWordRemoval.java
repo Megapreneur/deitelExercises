0c826292193c52ab6e916cb9b6d12b79a4bdd8c6
Megapreneur <ademolamegbabi@gmail.com>
2022-07-25 15:52:32 +0100

    Updated work

---
+++ src/DuplicateWordRemoval.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DuplicateWordRemoval {
+    public static void main(String[] args) {
+        String input = new Scanner(System.in).nextLine();
+
+        input = input.toLowerCase();
+        String[] elements = input.split(" ");
+
+        List<String> stringList = Stream.of(elements).sorted().distinct().toList();
+        System.out.println(stringList);
+    }
+}
---
+++ src/LamdbaExercise.java
@@ -0,0 +1,15 @@
+import java.security.SecureRandom;
+import java.util.stream.IntStream;
+
+public class LamdbaExercise {
+    public static void main(String[] args) {
+//        int result = IntStream.rangeClosed(1, 20).filter(x -> x % 2 != 0).map(x -> x * x * x).sum();
+//        System.out.println(result);
+//
+        int factorial = IntStream.rangeClosed(1, 99).reduce((acct, el) ->  acct * el).getAsInt();
+        System.out.println(factorial);
+
+//        SecureRandom secureRandom = new SecureRandom();
+//        secureRandom.ints(10, 1, 7).filter(x -> x % 2 == 0).forEach(System.out::println);
+    }
+}
