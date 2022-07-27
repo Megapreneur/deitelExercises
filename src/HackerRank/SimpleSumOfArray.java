775bc12e2fbdbec43f80d0b5ad450c6e78d4f13c
Megapreneur <ademolamegbabi@gmail.com>
2022-07-27 10:58:39 +0100

    Updated work

--- src/HackerRank/SimpleSumOfArray.java
+++ src/HackerRank/SimpleSumOfArray.java
@@ -1,15 +1,24 @@
 package HackerRank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleSumOfArray {
     public static void main(String[] args) {
-        int [] array ={1,2,3,4,10,11};
-        System.out.println(arraySum(array));
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(10);
+        list.add(11);
+        System.out.println(sumList(list));
 
     }
-    public static int arraySum(int[] arr){
+    public static int sumList(List<Integer>arr){
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+        for (int i = 0; i < arr.size(); i++) {
+            sum += arr.get(i);
         }
         return sum;
     }
